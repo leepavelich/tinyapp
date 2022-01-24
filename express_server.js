949d2cff4831da97398dee826b562ede5d8b81e6
Lee Pavelich <leepavelich@gmail.com>
2022-01-24 17:20:09 +0000

    Linted

--- express_server.js
+++ express_server.js
@@ -9,8 +9,8 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -29,13 +29,13 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
 app.listen(PORT, () => {
