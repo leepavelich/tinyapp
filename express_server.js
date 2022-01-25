d36b2786d825b24989be69b9ad35f455b4b3f6dc
Lee Pavelich <leepavelich@gmail.com>
2022-01-25 00:27:11 +0000

    Implemented logout logic

--- express_server.js
+++ express_server.js
@@ -51,6 +51,11 @@ app.post('/login', (req, res) => {
   res.redirect('/urls')
 })
 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username')
+  res.redirect('/urls')
+})
+
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
