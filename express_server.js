253751fce87f9c38f83b72846ee8b8c803d9d7d1
Lee Pavelich <leepavelich@gmail.com>
2022-01-25 00:17:32 +0000

    All rendered views pass the cookie username property

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ const PORT = 8080; // default port 8080
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
@@ -22,7 +23,10 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies.username,
+    urls: urlDatabase 
+  }; 
   res.render('urls_index', templateVars);
 });
 
@@ -52,7 +56,11 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    username: req.cookies.username,
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] 
+  };
   res.render('urls_show', templateVars);
 });
 
@@ -63,7 +71,10 @@ app.get('/u/:shortURL', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.render('404_page');
+  const templateVars = {
+    username: req.cookies.username,
+  }
+  res.render('404_page', templateVars);
 });
 
 app.listen(PORT, () => {
